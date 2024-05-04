aa55f6e6a7adf1c632627da3737371e00bd78520
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-05-04 16:38:48 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- app/Filament/Resources/AgencyResource/RelationManagers/MembersRelationManager.php
+++ app/Filament/Resources/AgencyResource/RelationManagers/MembersRelationManager.php
@@ -1,14 +1,22 @@
-<?php
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra Multi-Tenancy Admin Panel project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace App\Filament\Resources\AgencyResource\RelationManagers;
 
-use Filament\Forms;
+use App\Models\AgencyMember;
+use App\Models\User;
 use Filament\Forms\Form;
+use Filament\Forms\Components\Select;
 use Filament\Resources\RelationManagers\RelationManager;
-use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
 
 class MembersRelationManager extends RelationManager
 {
@@ -16,28 +24,39 @@ class MembersRelationManager extends RelationManager
 
     public function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('email')
-                    ->required()
-                    ->maxLength(255),
-            ]);
+        return $form->schema([
+            Select::make('user')
+                ->required()->multiple()
+                ->options(
+                    User::all()->except(
+                        AgencyMember::all()->map(
+                            fn (AgencyMember $member) => $member->user_id
+                        )->toArray()
+                    )->pluck('name', 'id')
+                ),
+        ]);
     }
 
     public function table(Table $table): Table
     {
         return $table
-            ->recordTitleAttribute('title')
+            ->recordTitleAttribute('name')
             ->columns([
-                Tables\Columns\TextColumn::make('email'),
+                TextColumn::make('name'),
+                TextColumn::make('email'),
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make()
+                CreateAction::make()
+                    ->createAnother(false)
+                    ->modalSubmitActionLabel(__('Assign'))
+                    ->modalHeading(__('Assign Members'))
                     ->label(__('Assign Members')),
             ])
             ->actions([
-                Tables\Actions\DeleteAction::make()
+                DeleteAction::make()
                     ->label(__('Remove Member')),
-            ]);
+            ])
+            ->emptyStateHeading(__('No members yet'))
+            ->emptyStateDescription(__('Assign a member to get started.'));
     }
 }
